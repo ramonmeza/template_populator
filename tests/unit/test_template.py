0ea5580dee78a5aa99c4123427466c399674142e
Ramon Meza <ramon_meza@live.com>
2022-02-09 22:23:03 -0500

    Reorganizing, attempts to fix import.

--- tests/unit/test_template.py
+++ tests/unit/test_template.py
@@ -1,14 +1,14 @@
-import src.template_populator.template
+import src.template
 
 TEST_FILE: str = '../data/test.template'
 
 
 def test_template_file_is_open_negative():
-    test = src.template_populator.template.Template()
+    test = src.template.Template()
     assert test._template_file_is_open() is False
 
 
 def test_template_file_is_open_positive():
-    test = src.template_populator.template.Template()
+    test = src.template.Template()
     test.load(TEST_FILE)
     assert test._template_file_is_open() is True
