53849f63f8a7b3842f58fb833b91ff30120ea96f
Ramon Meza <ramon_meza@live.com>
2022-02-09 22:12:52 -0500

    Add initial pytest

--- tests/unit/test_template.py
+++ tests/unit/test_template.py
@@ -1,2 +1,14 @@
-import pytest
+import template_populator.template
 
+TEST_FILE: str = '../data/test.template'
+
+
+def test_template_file_is_open_negative():
+    test = template_populator.template.Template()
+    assert test._template_file_is_open() is False
+
+
+def test_template_file_is_open_positive():
+    test = template_populator.template.Template()
+    test.load(TEST_FILE)
+    assert test._template_file_is_open() is True
