10e14d37d9488b968ac970b54c5f79c43b4aaf83
Ramon Meza <ramon_meza@live.com>
2022-02-10 18:53:03 -0500

    Remove bloated token tuple, added test case

--- src/template.py
+++ src/template.py
@@ -4,12 +4,10 @@ from typing import Dict, Iterator, List, Match
 import io
 import re
 
-
 class Template:
 
     # variables and types
-    Token = namedtuple("Token", "line_num start end")
-    _tokens: Dict[str, List[Token]]
+    _tokens: Dict[str, str]
     _token_pattern: str
     _file: io.TextIOWrapper
 
@@ -17,7 +15,7 @@ class Template:
 
     def __init__(self, token_pattern: str = '\\$\\{[a-zA-Z]*\\}') -> None:
         self._tokens = {
-            "global": []
+            "${Global}": []
         }
 
         self._token_pattern = token_pattern
@@ -27,31 +25,45 @@ class Template:
         if self._file_is_open():
             self._file.close()
 
-
     # public
     
-    def load(self, path: str):
+    def load(self, path: str) -> None:
         if self._file_is_open():
             self._file.close()
 
         self._file = open(path, 'r')
+        self._file.seek(0)
 
-    def scan(self):
+    def scan(self) -> None:
         if not self._file_is_open():
             msg = 'Not loaded template file, call load() first'
             raise FileNotFoundError(msg)
 
-        line_num: int = 0
+        self._file.seek(0)
         for line in self._file:
-            # increment line number
-            line_num += 1
-
             # get the token
             tokens = Template._scan_line(self._token_pattern, line)
 
             # add each token
             for token in tokens:
-                self._add_token(line_num, token)
+                self._add_token(token)
+
+    def replace(self, key: str, replacement: str) -> None:
+        if key in self._tokens:
+            for token in self._tokens[key]:
+                token = replacement
+
+    def render(self) -> io.StringIO:
+        rendered: io.StringIO = ''
+
+        self._file.seek(0)
+        for line in self._file:
+            tmp: str = line
+
+            for key in self._tokens.keys:
+                tmp = tmp.replace(key, self._tokens[key])
+
+        return rendered
 
     # private
 
@@ -61,14 +73,9 @@ class Template:
     def _scan_line(token_pattern: str, line: str) -> Iterator[Match]:
         return re.finditer(token_pattern, line)
 
-    def _add_token(self, line_num: int, match: Match):
+    def _add_token(self, match: Match):
         key = match.group(0)
-        token = Template.Token(line_num, match.start(), match.end())
 
         # create the list for the key
         if key not in self._tokens:
-            self._tokens[key] = []
-
-        # append the token if it is unique
-        if token not in self._tokens[key]:
-            self._tokens[key].append(token)
+            self._tokens[key] = ''
--- tests/unit/test_template.py
+++ tests/unit/test_template.py
@@ -12,6 +12,7 @@ import pytest
 import src.template
 
 TEST_FILE: str = 'tests/data/test.template'
+TEST_EXPECTED_FILE: str = 'tests/data/test_expected.template'
 
 
 @pytest.fixture()
@@ -25,22 +26,39 @@ def resource():
     # teardown test
     return
 
-
+# ensure the the file opened through Template.load()
+# is the same as a file we open manually
 def test_load(resource: src.template.Template):
     import os
     with open(TEST_FILE, 'r') as file:
         expected_stat = os.fstat(file.fileno())
-
         resource.load(TEST_FILE)
         test_stat = os.fstat(resource._file.fileno())
-
         assert test_stat == expected_stat
 
-
+# ensure that Template.scan() takes the loaded file,
+# scans it for tokens encapsulated with ${}, and adds
+# the tokens to it's internal map of tokens.
 def test_scan(resource: src.template.Template):
     resource.load(TEST_FILE)
     resource.scan()
+
     assert "${Namespace}" in resource._tokens
+    assert resource._tokens["${Namespace}"] == ''
     assert "${ClassName}" in resource._tokens
-    assert len(resource._tokens["${Namespace}"]) == 5
-    assert len(resource._tokens["${ClassName}"]) == 8
+    assert resource._tokens["${ClassName}"] == ''
+
+# ensure the Template.replace modified each token's
+# replacement attribute
+@pytest.mark.parametrize(
+    "key,replacement", [
+        ('${Namespace}', 'test_namespace'),
+        ('${ClassName}', 'TestClass')
+])
+def test_replace(resource: src.template.Template, key: str, replacement: str):
+    resource.load(TEST_FILE)
+    resource.scan()
+    resource.replace(key, replacement)
+
+    for token in resource._tokens[key]:
+        assert token.replacement == replacement
